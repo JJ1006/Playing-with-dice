d8b0de534c5cc8a2d8e78a4703c715baf2ac6134
Jaahanava Joshi <jaahanavajoshi@gmail.com>
2021-03-20 22:38:39 +0530

    We can get the output same as that of like a dice.
    We can avoid physically rolling a dice and instead run this code to get the output.

---
+++ output_of_dice.py
@@ -0,0 +1,15 @@
+
+import random
+ranNo = random.randint(1,7)           
+if ranNo==1:
+    print(f"The number rolled is :- {ranNo}")
+elif ranNo==2:
+    print(f"The number rolled is :- {ranNo}")
+elif ranNo==3:
+    print(f"The number rolled is :- {ranNo}")
+elif ranNo==4:
+    print(f"The number rolled is :- {ranNo}")
+elif ranNo==5:
+    print(f"The number rolled is :- {ranNo}")
+elif ranNo==6:
+    print(f"The number rolled is :- {ranNo}")
